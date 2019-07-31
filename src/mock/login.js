33082a8cec9f9d563ccf9d3fd27b911ad67bca46
yy <1023767856@qq.com>
2019-07-31 14:51:36 +0800

    finished history retain function

--- src/mock/login.js
+++ src/mock/login.js
@@ -15,17 +15,3 @@ const USER_MAP = {
         avator: 'https://avatars0.githubusercontent.com/u/20942571?s=460&v=4'
     }
 }
-
-export const login = req => {
-    req = JSON.parse(req.body)
-    return { token: USER_MAP[req.userName].token }
-}
-
-export const getUserInfo = req => {
-    const params = getParams(req.url)
-    return USER_MAP[params.token]
-}
-
-export const logout = req => {
-    return null
-}
